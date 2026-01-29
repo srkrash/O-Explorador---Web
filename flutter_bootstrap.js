6cbb1d55aaeba92cb9c5267190451316dc4ef436
Fay Klagenberg <capfay22@gmail.com>
2026-01-28 21:32:19 -0300

    rollback algoritmo captura gps

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3088228390"
+    serviceWorkerVersion: "3023611127"
   }
 });
